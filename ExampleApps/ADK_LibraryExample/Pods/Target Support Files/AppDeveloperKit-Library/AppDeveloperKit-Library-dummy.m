927476d08cb84d206bf9d2e27c92e3856e20e6aa
Scott Carter <scarter28m-github@yahoo.com>
2018-10-06 16:10:10 -0400

    Added FS_Status.nib

---
+++ ExampleApps/ADK_LibraryExample/Pods/Target Support Files/AppDeveloperKit-Library/AppDeveloperKit-Library-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_AppDeveloperKit_Library : NSObject
+@end
+@implementation PodsDummy_AppDeveloperKit_Library
+@end
---
+++ ExampleApps/ADK_LibraryExample/Pods/Target Support Files/Pods-ADK_LibraryExample/Pods-ADK_LibraryExample-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ADK_LibraryExample : NSObject
+@end
+@implementation PodsDummy_Pods_ADK_LibraryExample
+@end
